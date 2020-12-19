aa33bda12fe54ca191741d1b4f7c813d2b44423f
naomigrace <naomigracep@gmail.com>
2020-12-19 14:40:40 -0500

    max height enforced on cover image

--- components/cover-image.js
+++ components/cover-image.js
@@ -18,6 +18,9 @@ export default function CoverImage({
       <Link as={as} href={href} className="inline-block">
         <a aria-label={title}>
           <div
+            style={{
+              maxHeight: "14rem",
+            }}
             className={`h-full max-h-52 transition duration-500 bg-black hover:bg-purple-600 flex-1 sm:mx-0 max-w-xs relative rounded-xl cursor-pointer hover:shadow-lg`}
           >
             <div className="absolute inset-0 px-2 leading-tight text-center my-auto z-10 flex justify-center items-center">
